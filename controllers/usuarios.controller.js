b84a4beaccb8b89760217bb6edef51efb0c1348d
juliawadac <juliawadac@gmail.com>
2025-05-13 10:56:51 -0300

    13.05

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,4 +1,6 @@
 const mysql = require('../mysql');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.atualizarUsuario = async (req,res) => {
     try {
@@ -21,9 +23,7 @@ exports.atualizarUsuario = async (req,res) => {
     
 
     }catch (error) {
-        return res.status(500).send({
-            "mensagem": error
-        });
+        return res.status(500).send(error)
          
     } 
 
@@ -31,6 +31,7 @@ exports.atualizarUsuario = async (req,res) => {
 
 exports.cadastrarUsuario = async (req, res) => {
     try {
+        const hash = await bcrypt.hash(req.body.password, 10);
         const resultado = await mysql.execute(
             `insert into users (first_name, last_name, email, password, birth_date, phone)
             values (?, ?, ?, ?, ?, ?)`,
@@ -44,8 +45,39 @@ exports.cadastrarUsuario = async (req, res) => {
 
 
     } catch (error) {
-        return res.status(500).send({
-            "mensagem": error
-        });
+        return res.status(500).send(error)
     }
 }
+
+
+exports.login = async (req, res) => {
+    try {
+      const usuario = await mysql.execute(
+        `SELECT * FROM users WHERE email = ?`,
+        [req.body.email]);
+  
+      if (usuario.length == 0) {
+        return res.status(401).send({ "Mensagem": "Usuario não cadastrado" });
+      }
+  
+      const match = await bcrypt.compare(req.body.password, usuario[0].password);
+      if (!match) {
+        return res.status(401).send({ "Mensagem": "Senha incorreta!" })
+      }
+  
+      const token = jwt.sign({
+        id: usuario[0].id,
+        first_name: usuario[0].first_name,
+        last_name: usuario[0].last_name,
+        email: usuario[0].email,
+        birth_date: usuario[0].birth_date,
+      }, "senhadojwt");
+      return res.status(200).send({
+        "Mensagem": "Usuario não autenticado com sucesso",
+        "token": token
+      });
+  
+    } catch (error) {
+      return res.status(500).send(error)
+    }
+  }
---
+++ routes/filas.route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const login = require("../middleware.js/usuarios.middlewre");
+const filaController = require("../controllers/filascontroller");
+
+router.post("/:idRide",
+     login.required, filaController.verificarFilas, filaController.entrarFila);
+
+module.exports = router;
